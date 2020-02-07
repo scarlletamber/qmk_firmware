237a0b33e77cbf7d3a0a8bc3a10645789b8c9eeb
notePC_amber <senjyu0626@yahoo.co.jp>
2020-02-07 15:05:08 +0900

    attack25の追加

--- keyboards/shinobigatana53/keymaps/default/keymap.c
+++ keyboards/shinobigatana53/keymaps/default/keymap.c
@@ -39,12 +39,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( /* Base */
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , JP_LBRC,         JP_RBRC, KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
 		KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_MINS,         JP_CIRC, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
-		KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_ESC , KC_ESC, KC_BSPC, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
+		KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_DEL , KC_ESC, KC_BSPC, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
 		KC_ESC , KC_LGUI, KC_APP , KC_LALT,                   KC_LOEN ,        KC_RASP,       KC_RALT   , KC_APP , KC_RGUI, KC_RCTL
   ),
 
   [_LOWER] = LAYOUT(
-		KC_GRV , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, JP_LCBR,         JP_RCBR, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, JP_CIRC, KC_AT  ,
+		KC_GRV , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, JP_LCBR,         JP_RCBR, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, JP_CIRC, JP_AT  ,
 		_______, JP_GRV , JP_BSLS, KC_MINS, JP_EQL , JP_LBRC, JP_EQL ,         JP_TILD, JP_RBRC, JP_SCLN, JP_ASTR, KC_SLSH, JP_PLUS, JP_ASTR,
 		_______, _______, JP_PIPE, JP_UNDS, JP_PLUS, JP_LCBR, _______, _______,_______, JP_RCBR, JP_COLN, JP_LT  , JP_GT  , JP_QUES, _______,
 		_______, _______, _______, _______,                   _______,         _______,          _______, _______, _______, _______ ),

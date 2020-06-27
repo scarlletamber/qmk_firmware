1f3904ca5650b1dce8777a655b2aecd66de86f5c
amber <senjyu0626@yahoo.co.jp>
2020-06-28 00:40:10 +0900

    忍刀53のキーマップ変更

--- keyboards/shinobigatana53/keymaps/default/config.h
+++ keyboards/shinobigatana53/keymaps/default/config.h
@@ -14,6 +14,18 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#pragma once
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
 
-// place overrides here
+#include "../../config.h"
+
+#define TAPPING_TERM 150
+
+//#undef RGBLED_NUM
+//#define RGBLIGHT_ANIMATIONS
+//#define RGBLED_NUM 7
+//#define RGBLIGHT_HUE_STEP 8
+//#define RGBLIGHT_SAT_STEP 8
+//#define RGBLIGHT_VAL_STEP 8
+
+#endif
--- keyboards/shinobigatana53/keymaps/default/keymap.c
+++ keyboards/shinobigatana53/keymaps/default/keymap.c
@@ -34,6 +34,7 @@ enum custom_keycodes {
 
 #define KC_LOEN LT(_LOWER, KC_SPC)
 #define KC_RASP LT(_RAISE, KC_ENT)
+#define ADJT MO(_ADJUST)  		// Hold for ADJUST
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( /* Base */
@@ -47,28 +48,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GRV , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, JP_LCBR,         JP_RCBR, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, JP_CIRC, JP_AT  ,
 		_______, JP_GRV , JP_BSLS, KC_MINS, JP_EQL , JP_LBRC, JP_EQL ,         JP_TILD, JP_RBRC, JP_SCLN, JP_ASTR, KC_SLSH, JP_PLUS, JP_ASTR,
 		_______, _______, JP_PIPE, JP_UNDS, JP_PLUS, JP_LCBR, _______, _______,_______, JP_RCBR, JP_COLN, JP_LT  , JP_GT  , JP_QUES, _______,
-		_______, _______, _______, _______,                   _______,         _______,          _______, _______, _______, _______ ),
+		_______, _______, _______, _______,                   _______,         ADJT,          _______, _______, _______, _______ ),
 
   [_RAISE] = LAYOUT(
 		KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , _______,          _______, KC_F6 , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
 		_______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , _______,          _______, KC_F4 , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX,
 		_______, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______, _______, _______, KC_F10, KC_F11 , KC_F12 , _______, _______ , _______,
+		_______, _______, _______, _______,                   ADJT,          _______,         _______, _______, _______, _______ ),
+
+ [_ADJUST] = LAYOUT(
+		RESET  , _______, KC_PSCR, KC_SLCK, KC_PAUS, _______, _______,          _______, RGB_TOG, RGB_HUD, RGB_VAD, RGB_SAD, KC_F10 , KC_F11 ,
+		_______, _______, _______, _______, _______, _______, _______,          _______, _______, RGB_MOD, RGB_HUI, RGB_VAI, RGB_SAI, XXXXXXX,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ , _______,
 		_______, _______, _______, _______,                   _______,          _______,         _______, _______, _______, _______ ),
 
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+  }
   return true;
-}
-
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
-void led_set_user(uint8_t usb_led) {
-
 }
